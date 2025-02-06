99b7fae675bc87710debcb0770ae3987a9cd6239
Anish-Kumar12 <anishkumar344567@gmail.com>
2025-02-06 16:09:42 +0530

    Updated MeetingRoom ui and Functionality

--- src/actions/stream.actions.ts
+++ src/actions/stream.actions.ts
@@ -1,20 +1,24 @@
-
 "use server";
 
 import { currentUser } from "@clerk/nextjs/server";
 import { StreamClient } from "@stream-io/node-sdk";
 
 export const streamTokenProvider = async () => {
-  const user = await currentUser();
+  try {
+    const user = await currentUser();
 
-  if (!user) throw new Error("User not authenticated");
+    if (!user) throw new Error("User not authenticated");
 
-  const streamClient = new StreamClient(
-    process.env.NEXT_PUBLIC_STREAM_API_KEY!,
-    process.env.STREAM_SECRET_KEY!
-  );
+    const streamClient = new StreamClient(
+      process.env.NEXT_PUBLIC_STREAM_API_KEY!,
+      process.env.STREAM_SECRET_KEY!
+    );
 
-  const token = streamClient.generateUserToken({ user_id: user.id });
+    const token = streamClient.generateUserToken({ user_id: user.id });
 
-  return token;
+    return token;
+  } catch (error) {
+    console.error("Error in streamTokenProvider:", error);
+    throw error;
+  }
 };
